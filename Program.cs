9d49127da632d72e3b354c3c20950382868ee31a
kavita041096 <39227558+kavita041096@users.noreply.github.com>
2019-06-17 11:13:06 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace pipelines_dotnet_core
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            Console.WriteLine("Hello World!");
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
